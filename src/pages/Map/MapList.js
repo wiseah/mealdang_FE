fa5f15a6c9410f1c838e4f6cbd0f4d7836e689a8
wiseah <hyunah2765@gmail.com>
2024-07-24 04:39:15 +0900

    [#17] 더미데이터로 맵 연동 테스트

---
+++ src/pages/Map/MapList.js
@@ -0,0 +1,13 @@
+export const dummyData = {
+    "cafe": [
+      { "latitude": 37.564213, "longitude": 127.001699 },
+      { "latitude": 37.579616, "longitude": 126.977041 },
+      { "latitude": 37.550556, "longitude": 126.991251 }
+    ],
+    "restaurant": [
+      { "latitude": 37.556400, "longitude": 126.925641 },
+      { "latitude": 37.564093, "longitude": 127.001647 },
+      { "latitude": 37.568595, "longitude": 126.976977 }
+    ]
+  };
+  
