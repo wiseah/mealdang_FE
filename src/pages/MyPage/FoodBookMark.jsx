d2ab201217469ae984748f6216f241bfc832ee97
tlstkdgus <tlstkdgus9@naver.com>
2024-07-24 14:04:14 +0900

    중간수정

--- src/pages/MyPage/FoodBookMark.jsx
+++ src/pages/MyPage/FoodBookMark.jsx
@@ -17,7 +17,17 @@ const Introduction = styled.div`
     font-weight: 400;
     text-align: center;
 `
-
+const Date = styled.div`
+    width: 160px;
+    color: #000;
+    font-family: "Wavve PADO TTF";
+    font-size: 30px;
+    font-weight: 400;
+    align-self: flex-start;
+    padding-left: 30px;
+    padding-top: 20px;
+    padding-bottom: 10px;
+`
 
 
 export function FoodBookMark(){
@@ -27,6 +37,7 @@ export function FoodBookMark(){
             <Introduction>
                 여러분들이 맛있게 먹었던 <br/>식단을 두고두고 볼 수 있어요
             </Introduction>
+            <Date>2024.07.13</Date>
             <FoodRecommendBack/>
         </Container>
     )
