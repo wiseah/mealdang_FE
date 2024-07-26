4c3df7d8a1911bd53a1c63683837d4470a485988
wiseah <hyunah2765@gmail.com>
2024-07-26 22:30:50 +0900

    [#17] 혈당 맵 페이지 구현완료

--- src/pages/Map/MapList.js
+++ src/pages/Map/MapList.js
@@ -1,56 +1,60 @@
 export const dummyData = {
-  cafe: [
-    {
-      // 스타벅스 광화문점
-      latitude: 37.570881,
-      longitude: 126.976933
-    },
-    {
-      // 테라로사 한남점
-      latitude: 37.53514257044817,
-      longitude: 127.00774845781423
-    },
-    {
-      // 블루보틀 삼청동점
-      latitude: 37.58264348952434,
-      longitude: 126.98409463271352
-    },
-    {
-      // 할리스커피 서울역점
-      latitude: 37.5547,
-      longitude: 126.9706
-    },
-    {
-      // 빽다방 명동점
-      latitude: 37.5639,
-      longitude: 126.9827
-    }
-  ],
-  restaurant: [
-    {
-      // 명동교자 본점
-      latitude: 37.56374669937975,
-      longitude: 126.98524650409275
-    },
-    {
-      // 을지로 미쓰리
-      latitude: 37.56635749961843,
-      longitude: 126.99098550409174
-    },
-    {
-      // 서울식당
-      latitude: 37.57257234028572,
-      longitude: 126.97730649961853
-    },
-    {
-      // 서래갈매기 강남점
-      latitude: 37.498,
-      longitude: 127.0276
-    },
-    {
-      // 한우물회 마포점
-      latitude: 37.5535,
-      longitude: 126.935
-    }
-  ]
+	"cafe": [
+		{
+			"latitude": 37.570881,
+			"longitude": 126.976933,
+			"place_name": "스타벅스 광화문점",
+			"place_link": "https://place.map.kakao.com/7961649",
+			"road_name_address": "서울 종로구 세종대로 167",
+			"lot_number_address": "서울 종로구 세종로 211",
+			"phone": "02-737-0070"
+		},
+		{
+			"latitude": 37.53514257044817,
+			"longitude": 127.00774845781423,
+			"place_name": "테라로사 한남점",
+			"place_link": "https://place.map.kakao.com/27079320",
+			"road_name_address": "서울 용산구 이태원로 225",
+			"lot_number_address": "서울 용산구 한남동 683-142",
+			"phone": "02-796-7000"
+		},
+		{
+			"latitude": 37.58264348952434,
+			"longitude": 126.98409463271352,
+			"place_name": "블루보틀 삼청동점",
+			"place_link": "https://place.map.kakao.com/353512935",
+			"road_name_address": "서울 종로구 북촌로5가길 7",
+			"lot_number_address": "서울 종로구 삼청동 35-67",
+			"phone": "1533-6906"
+		}
+	],
+	"restaurant": [
+		{
+			"latitude": 37.56374669937975,
+			"longitude": 126.98524650409275,
+			"place_name": "명동교자 본점",
+			"place_link": "https://place.map.kakao.com/10332413",
+			"road_name_address": "서울 중구 명동10길 29",
+			"lot_number_address": "명동2가 25-36",
+			"phone": "02-776-5348"
+		},
+		{
+			"latitude": 37.56635749961843,
+			"longitude": 126.99098550409174,
+			"place_name": "을지로 미쓰리",
+			"place_link": "https://place.map.kakao.com/17557418",
+			"road_name_address": "서울 중구 을지로11길 26",
+			"lot_number_address": "서울 중구 을지로3가 104-5",
+			"phone": "02-777-3321"
+		},
+		{
+			"latitude": 37.57257234028572,
+			"longitude": 126.97730649961853,
+			"place_name": "서울식당",
+			"place_link": "https://place.map.kakao.com/9256066",
+			"road_name_address": "서울 종로구 사직로 130",
+			"lot_number_address": "서울 종로구 사직동 9-1",
+			"phone": "02-736-8080"
+		}
+	]
 };
