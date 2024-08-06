ae683f47be4eb2e01128f799cc4c0f8845b31144
wiseah <hyunah2765@gmail.com>
2024-08-06 16:32:16 +0900

    [api] 배경전역설정

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import routes from './routes.js';
 import Mobile from './styles/Mobile.js';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    height: 100%;
+    background-position: center;
+    background-size: cover;
+    background-image: url('/images/backgroundImg.png');
+    background-attachment: fixed; // Add this line to make the background fixed
+`;
 
 function App() {
   const elements = routes.map((item, index) => (
     <Route key={index} path={item.path} element={item.element} />
   ));
   return (
-    <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Mobile/>}>
-          {elements}
-        </Route>
-      </Routes>
-    </Router>
-    </>
+    <Container>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Mobile/>}>
+            {elements}
+          </Route>
+        </Routes>
+      </Router>
+    </Container>
   );
 }
 
